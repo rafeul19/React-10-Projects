6ddc3ee75dbc4868be5efccf3edd4bd3fa15187a
RAFE <rafe.19.dev@gmail.com>
2025-04-10 22:22:59 +0600

    All my files for new project added  in this project

---
+++ project_3/src/App.jsx
@@ -0,0 +1,12 @@
+import StartGame from "./components/StartGame";
+
+function App() {
+  return (
+    <>
+      <StartGame />
+      
+    </>
+  )
+}
+
+export default App;
---
+++ project_3/src/components/StartGame.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const StartGame = () => {
+  return <div>Start Game</div>;
+};
+
+export default StartGame;
